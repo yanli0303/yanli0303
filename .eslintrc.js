41d27f5e094e15d8a9c4e00d57501214d4dd8fa5
Peter Li <peterleepersonal@gmail.com>
2020-08-13 13:52:19 -0400

    init dev env

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  globals: {
+    node: true
+  },
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  settings: {
+    react: {
+      version: '999.999.999'
+    }
+  }
+}
