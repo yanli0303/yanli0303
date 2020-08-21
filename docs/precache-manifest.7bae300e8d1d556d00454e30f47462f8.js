63d24502ebf0061cdad776b07e6e4c717a4775e1
Peter Li <peterleepersonal@gmail.com>
2020-08-21 17:58:57 -0400

    feat(resume): reset.css

---
+++ docs/precache-manifest.7bae300e8d1d556d00454e30f47462f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399876735ce481923c8aad411fb768a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ebd5af9ed44df3c7928",
+    "url": "./static/css/main.0252e454.chunk.css"
+  },
+  {
+    "revision": "b21e7209ab3f8a0dc78e",
+    "url": "./static/js/2.f243ac28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f243ac28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebd5af9ed44df3c7928",
+    "url": "./static/js/main.ed0cfa82.chunk.js"
+  },
+  {
+    "revision": "2207813b094fb70c64c4",
+    "url": "./static/js/runtime-main.70050e21.js"
+  }
+]);
