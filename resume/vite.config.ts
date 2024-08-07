8bebdd52cb865e6c78914bbce8d0fd094bf93b15
Peter Li <peterleepersonal@gmail.com>
2024-08-07 01:34:15 -0400

    update pages

--- resume/vite.config.ts
+++ resume/vite.config.ts
@@ -3,13 +3,13 @@ import { type UserConfig, defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(
-	async (): Promise<UserConfig> => ({
-		plugins: [react()],
-		base: "",
-		build: {
-			emptyOutDir: false,
-			outDir: "docs",
-			assetsDir: "",
-		},
-	}),
+  async (): Promise<UserConfig> => ({
+    plugins: [react()],
+    base: "",
+    build: {
+      emptyOutDir: true,
+      outDir: "../docs",
+      assetsDir: "",
+    },
+  }),
 );
