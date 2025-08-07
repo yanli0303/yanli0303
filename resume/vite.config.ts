65bebfddcbd9d2b0d65587c1d2d7a45732e38611
Peter Li <peterleepersonal@gmail.com>
2025-08-07 14:52:39 -0400

    dev(lint): lint resume project

--- resume/vite.config.ts
+++ resume/vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { type UserConfig, defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(
