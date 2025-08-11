60eb2a1a5b35813bee7a5eb190e670f58b240b79
Peter Li <peterleepersonal@gmail.com>
2025-08-10 23:47:28 -0400

    feat(resume): update skills

--- resume/src/Skills.tsx
+++ resume/src/Skills.tsx
@@ -1,27 +1,20 @@
-import { SectionHeader } from './SectionHeader';
+import { SectionHeader } from "./SectionHeader";
 
 export const Skills = () => (
   <section>
     <SectionHeader title="Skills" />
-    <div className="columns is-2">
+    <div className="columns is-1">
       <div className="column is-2 has-text-weight-bold">Highly Proficient</div>
       <div className="column">
-        Front-end web development in JS/TS with React and Redux.
+        Serverless/cloud architectures, distributed system design, microservices, SaaS, containerization, web security,
+        HTTP and OAuth 2.0 protocols, caching mechanisms, and databases.
       </div>
     </div>
-    <div className="columns is-2">
+    <div className="columns is-1">
       <div className="column is-2 has-text-weight-bold">Proficient</div>
       <div className="column">
-        HTTP and REST protocols, design principles, caching mechanisms, and databases.
-      </div>
-    </div>
-    <div className="columns is-2">
-      <div className="column is-2 has-text-weight-bold">
-        Approaching Proficient
-      </div>
-      <div className="column">
-        Distributed system design, project management, mobile/desktop app
-        development with Swift and Electron, and web server operations.
+        LLMs, supervised fine-tuning and RAG. Project management, CI/CD pipelines, i18n, and DevOps. SPA, PWA,
+        Full-stack and cross-platform development (web, browser extension, desktop, mobile).
       </div>
     </div>
   </section>
